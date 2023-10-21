1a2960e4013adf60ff794db2051a7b77c1923614
Viktor_Debre <viktor.debre2002@gmail.com>
2023-10-21 23:50:47 +0200

    Add Arithmetic encoding prototype

--- Presentation/Lab1/HuffmanPresentation.cs
+++ Presentation/Lab1/HuffmanPresentation.cs
@@ -1,5 +1,8 @@
-﻿using Lab1.HuffmanCode.Huffman;
+﻿using Lab1.HuffmanCode.Arithmetic;
+using Lab1.HuffmanCode.Huffman;
 using Lab1.HuffmanCode.Nodes;
+using Lab1.HuffmanCode.ResultComparer;
+using System.Numerics;
 
 namespace DataCompressionLabs.Presentation.Lab1;
 
@@ -7,10 +10,11 @@ public class HuffmanPresentation : Presentation
 {
     public void Execute()
     {
+        //Huffman compression
         var encoder = new HuffmanEncoder();
         var decoder = new HuffmanDecoder();
 
-        string inputText = "this is an example for huffman encoding qwertyuiopwertyuioqwertyuiartyuiqwertywqertyuiqwertywq";
+        string inputText = "this is an example for huffman encoding";
 
         Dictionary<char, int> charFrequencies = inputText
             .GroupBy(c => c)
@@ -23,11 +27,24 @@ public class HuffmanPresentation : Presentation
 
         Console.WriteLine("Original Text: " + inputText);
         Console.WriteLine("Encoded Data: " + encodedData);
-        var compressionRatio = inputText.Length * 2 * 8 * 1.0f / encodedData.Length;
+        var compressionRatio = CompressionComparer.CompressionRatio(inputText, encodedData);
         Console.WriteLine("Compression ratio: " + compressionRatio);
         var decodedData = HuffmanDecoder.Decode(huffmanTree, encodedData);
         Console.WriteLine("Decoded Data: " + decodedData);
+        Console.WriteLine("Decoded text is similar: " + CompressionComparer.IsTextsSimilar(inputText, decodedData));
 
-        
+        //Arithmetic compression
+        var arithmeticEncoder = new ArithmeticDataEncoder();
+        var radix = 10;
+        //Dictionary<char, double> symbolProbabilities = arithmeticEncoder.CalculateSymbolProbabilities(inputText);
+        Console.WriteLine("Original Text: " + inputText);
+        Tuple<BigInteger, int, Dictionary<char, long>> encoded = arithmeticEncoder.ArithmeticCoding(inputText, 10);
+        //string compressedData = arithmeticEncoder.Encode(encoded);
+        //Console.WriteLine("Compressed Data: " + compressedData);
+        //compressionRatio = CompressionComparer.CompressionRatio(inputText, compressedData);
+        Console.WriteLine("Compression ratio: " + compressionRatio);
+        string decompressedData = arithmeticEncoder.ArithmeticDecoding(encoded.Item1, radix, encoded.Item2, encoded.Item3);
+        Console.WriteLine("Decompressed Data: " + decompressedData);
+        Console.WriteLine("Decoded text is similar: " + CompressionComparer.IsTextsSimilar(inputText, decompressedData));
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,28 +1,180 @@
-﻿using DataCompressionLabs.Presentation.Lab1;
+﻿//using DataCompressionLabs.Presentation.Lab1;
 
-while (true)
+//while (true)
+//{
+//    Console.WriteLine(
+//        @"Greetings, what lab are you want to run:
+//        press: 1-huffman, 2-?, 3-?, 4-?");
+
+//    var lab = Console.ReadLine();
+//    var executeLab = lab switch
+//    {
+//        "1" => new HuffmanPresentation(),
+//        "2" => new HuffmanPresentation(),
+//        "3" => new HuffmanPresentation(),
+//        "4" => new HuffmanPresentation(),
+//        _ => null
+//    };
+//    if (executeLab != null)
+//    {
+//        executeLab.Execute();
+//    }
+//    Console.WriteLine(
+//       @"Do you want to continue:
+//       y-yes, anything else - no");
+//    var programContinue = Console.ReadLine();
+//    if (programContinue == "y")
+//    {
+//        Console.Clear();
+//        continue;
+//    }
+//    break;
+//}
+using Lab1.HuffmanCode.ResultComparer;
+using System.Numerics;
+using System.Text;
+
+namespace AruthmeticCoding
 {
-    Console.WriteLine(
-        @"Greetings, what lab are you want to run:
-        press: 1-huffman, 2-?, 3-?, 4-?");
+    using Freq = Dictionary<char, long>;
+    using Triple = Tuple<BigInteger, int, Dictionary<char, long>>;
 
-    var lab = Console.ReadLine();
-    var executeLab = lab switch
+    class Program
     {
-        "1" => new HuffmanPresentation(),
-        "2" => new HuffmanPresentation(),
-        "3" => new HuffmanPresentation(),
-        "4" => new HuffmanPresentation(),
-        _ => null
-    };
-    if (executeLab != null)
-    {
-        executeLab.Execute();
+        static Freq CumulativeFreq(Freq freq)
+        {
+            long total = 0;
+            Freq cf = new Freq();
+            for (int i = 0; i < 256; i++)
+            {
+                char c = (char)i;
+                if (freq.ContainsKey(c))
+                {
+                    long v = freq[c];
+                    cf[c] = total;
+                    total += v;
+                }
+            }
+            return cf;
+        }
+
+        static Triple ArithmeticCoding(string str, long radix)
+        {
+            // The frequency of characters
+            Freq freq = new Freq();
+            foreach (char c in str)
+            {
+                if (freq.ContainsKey(c))
+                {
+                    freq[c] += 1;
+                }
+                else
+                {
+                    freq[c] = 1;
+                }
+            }
+
+            // The cumulative frequency
+            Freq cf = CumulativeFreq(freq);
+
+            // Base
+            BigInteger @base = str.Length;
+
+            // Lower bound
+            BigInteger lower = 0;
+
+            // Product of all frequencies
+            BigInteger pf = 1;
+
+            // Each term is multiplied by the product of the
+            // frequencies of all previously occuring symbols
+            foreach (char c in str)
+            {
+                BigInteger x = cf[c];
+                lower = lower * @base + x * pf;
+                pf = pf * freq[c];
+            }
+
+            // Upper bound
+            BigInteger upper = lower + pf;
+
+            int powr = 0;
+            BigInteger bigRadix = radix;
+
+            while (true)
+            {
+                pf = pf / bigRadix;
+                if (pf == 0) break;
+                powr++;
+            }
+
+            BigInteger diff = (upper - 1) / (BigInteger.Pow(bigRadix, powr));
+            return new Triple(diff, powr, freq);
+        }
+
+        static string ArithmeticDecoding(BigInteger num, long radix, int pwr, Freq freq)
+        {
+            BigInteger powr = radix;
+            BigInteger enc = num * BigInteger.Pow(powr, pwr);
+            long @base = freq.Values.Sum();
+
+            // Create the cumulative frequency table
+            Freq cf = CumulativeFreq(freq);
+
+            // Create the dictionary
+            Dictionary<long, char> dict = new Dictionary<long, char>();
+            foreach (char key in cf.Keys)
+            {
+                long value = cf[key];
+                dict[value] = key;
+            }
+
+            // Fill the gaps in the dictionary
+            long lchar = -1;
+            for (long i = 0; i < @base; i++)
+            {
+                if (dict.ContainsKey(i))
+                {
+                    lchar = dict[i];
+                }
+                else if (lchar != -1)
+                {
+                    dict[i] = (char)lchar;
+                }
+            }
+
+            // Decode the input number
+            StringBuilder decoded = new StringBuilder((int)@base);
+            BigInteger bigBase = @base;
+            for (long i = @base - 1; i >= 0; --i)
+            {
+                BigInteger pow = BigInteger.Pow(bigBase, (int)i);
+                BigInteger div = enc / pow;
+                char c = dict[(long)div];
+                BigInteger fv = freq[c];
+                BigInteger cv = cf[c];
+                BigInteger diff = enc - pow * cv;
+                enc = diff / fv;
+                decoded.Append(c);
+            }
+
+            // Return the decoded output
+            return decoded.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            long radix = 10;
+            string inputText = "this is an example for huffman encoding";
+            Console.WriteLine("Original Text: " + inputText);
+
+            Triple encoded = ArithmeticCoding(inputText, radix);
+            string dec = ArithmeticDecoding(encoded.Item1, radix, encoded.Item2, encoded.Item3);
+            Console.WriteLine("{0,200} * {1}^{2}", encoded.Item1, radix, encoded.Item2);
+            Console.WriteLine("Decoded text is similar: " + CompressionComparer.IsTextsSimilar(inputText, dec));
+
+            var compressionRate = inputText.Length * 2 * 8 * 1.0f / (double)BigInteger.Log2(encoded.Item1);
+            Console.WriteLine("Compression rate: {0:F6}", compressionRate);
+        }
     }
-    Console.WriteLine(
-       @"Do you want to continue:
-       y-yes, n-no");
-    var programContinue = Console.ReadLine();
-    if (programContinue == "n") break;
-    Console.Clear();
 }
