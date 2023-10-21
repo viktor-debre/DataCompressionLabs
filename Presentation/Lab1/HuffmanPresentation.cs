4dfe7f7195fd407a77c20f29a9be4d07c7650a2f
Viktor_Debre <viktor.debre2002@gmail.com>
2023-10-21 22:25:11 +0200

    huffman base realization

---
+++ Presentation/Lab1/HuffmanPresentation.cs
@@ -0,0 +1,33 @@
+﻿using Lab1.HuffmanCode.Huffman;
+using Lab1.HuffmanCode.Nodes;
+
+namespace DataCompressionLabs.Presentation.Lab1;
+
+public class HuffmanPresentation : Presentation
+{
+    public void Execute()
+    {
+        var encoder = new HuffmanEncoder();
+        var decoder = new HuffmanDecoder();
+
+        string inputText = "this is an example for huffman encoding qwertyuiopwertyuioqwertyuiartyuiqwertywqertyuiqwertywq";
+
+        Dictionary<char, int> charFrequencies = inputText
+            .GroupBy(c => c)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        HuffmanNode huffmanTree = encoder.BuildHuffmanTree(charFrequencies);
+        Dictionary<char, string> huffmanCodes = encoder.BuildHuffmanCodes(huffmanTree);
+
+        string encodedData = encoder.Encode(inputText, huffmanCodes);
+
+        Console.WriteLine("Original Text: " + inputText);
+        Console.WriteLine("Encoded Data: " + encodedData);
+        var compressionRatio = inputText.Length * 2 * 8 * 1.0f / encodedData.Length;
+        Console.WriteLine("Compression ratio: " + compressionRatio);
+        var decodedData = HuffmanDecoder.Decode(huffmanTree, encodedData);
+        Console.WriteLine("Decoded Data: " + decodedData);
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using DataCompressionLabs.Presentation.Lab1;
+
+while (true)
+{
+    Console.WriteLine(
+        @"Greetings, what lab are you want to run:
+        press: 1-huffman, 2-?, 3-?, 4-?");
+
+    var lab = Console.ReadLine();
+    var executeLab = lab switch
+    {
+        "1" => new HuffmanPresentation(),
+        "2" => new HuffmanPresentation(),
+        "3" => new HuffmanPresentation(),
+        "4" => new HuffmanPresentation(),
+        _ => null
+    };
+    if (executeLab != null)
+    {
+        executeLab.Execute();
+    }
+    Console.WriteLine(
+       @"Do you want to continue:
+       y-yes, n-no");
+    var programContinue = Console.ReadLine();
+    if (programContinue == "n") break;
+    Console.Clear();
+}
