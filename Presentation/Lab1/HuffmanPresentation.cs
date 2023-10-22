5ece5d1785fb3154ce32c2edaa84bf7b0a347ff8
Viktor_Debre <viktor.debre2002@gmail.com>
2023-10-22 09:46:53 +0200

    new setup

--- Presentation/Lab1/HuffmanPresentation.cs
+++ Presentation/Lab1/HuffmanPresentation.cs
@@ -14,7 +14,10 @@ public class HuffmanPresentation : Presentation
         var encoder = new HuffmanEncoder();
         var decoder = new HuffmanDecoder();
 
-        string inputText = "this is an example for huffman encoding";
+        string inputText = @"this is an example for huffman encoding this is an example for huffman encoding
+        this is an example for huffman encodingthis is an example for huffman encodingthis is an example for huffman encodingthis is an example for huffman encoding
+        this is an example for huffman encodingthis is an example for huffman encodingthis is an example for huffman encoding
+        qwetSDFasbhnghbfxc";
 
         Dictionary<char, int> charFrequencies = inputText
             .GroupBy(c => c)
@@ -28,23 +31,21 @@ public class HuffmanPresentation : Presentation
         Console.WriteLine("Original Text: " + inputText);
         Console.WriteLine("Encoded Data: " + encodedData);
         var compressionRatio = CompressionComparer.CompressionRatio(inputText, encodedData);
-        Console.WriteLine("Compression ratio: " + compressionRatio);
+        Console.WriteLine("Compression ratio: {0:F6}", compressionRatio);
         var decodedData = HuffmanDecoder.Decode(huffmanTree, encodedData);
         Console.WriteLine("Decoded Data: " + decodedData);
         Console.WriteLine("Decoded text is similar: " + CompressionComparer.IsTextsSimilar(inputText, decodedData));
 
-        //Arithmetic compression
-        var arithmeticEncoder = new ArithmeticDataEncoder();
-        var radix = 10;
-        //Dictionary<char, double> symbolProbabilities = arithmeticEncoder.CalculateSymbolProbabilities(inputText);
+        long radix = 10;
+        //string inputText = "this is an example for huffman encoding";
         Console.WriteLine("Original Text: " + inputText);
-        Tuple<BigInteger, int, Dictionary<char, long>> encoded = arithmeticEncoder.ArithmeticCoding(inputText, 10);
-        //string compressedData = arithmeticEncoder.Encode(encoded);
-        //Console.WriteLine("Compressed Data: " + compressedData);
-        //compressionRatio = CompressionComparer.CompressionRatio(inputText, compressedData);
-        Console.WriteLine("Compression ratio: " + compressionRatio);
-        string decompressedData = arithmeticEncoder.ArithmeticDecoding(encoded.Item1, radix, encoded.Item2, encoded.Item3);
-        Console.WriteLine("Decompressed Data: " + decompressedData);
-        Console.WriteLine("Decoded text is similar: " + CompressionComparer.IsTextsSimilar(inputText, decompressedData));
+
+        Tuple<BigInteger, int, Dictionary<char, long>> encoded = ArithmeticDataEncoder.ArithmeticCoding(inputText, radix);
+        string dec = ArithmeticDataDecoder.ArithmeticDecoding(encoded.Item1, radix, encoded.Item2, encoded.Item3);
+        Console.WriteLine("{0} * {1}^-{2}", encoded.Item1, radix, encoded.Item2);
+        Console.WriteLine("Decoded text is similar: " + CompressionComparer.IsTextsSimilar(inputText, dec));
+
+        var compressionRate = inputText.Length * 2 * 8 * 1.0f / (double)BigInteger.Log2(encoded.Item1);
+        Console.WriteLine("Compression rate: {0:F6}", compressionRate);
     }
 }
